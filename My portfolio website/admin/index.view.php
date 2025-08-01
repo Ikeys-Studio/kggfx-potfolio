f7edd2e7d2bb8ef58428a9bd351e64c6fe50d24e
Kofi Grooves <ikeysstudio@gmail.com>
2025-08-01 08:08:25 +0000

    Delete My portfolio website directory

--- My portfolio website/admin/index.view.php
+++
@@ -1,7 +0,0 @@
-<h2>Welcome, Admin</h2>
-<ul>
-  <li><a href="../pages/projects.php">Manage Projects</a></li>
-  <li><a href="../pages/visitors.php">View Insight</a></li>
-  <li><a href="../pages/messages.php">View Messages</a></li>
-  <li><a href="logout.php">Logout</a></li>
-</ul>
